d6c3fbfb1e7d021361fe85170e627ae86243aae4
the-black-wizards <emperor@initia.co>
2023-02-27 16:50:21 +0900

    update js scripts

--- js/scripts/prepare.sh
+++ js/scripts/prepare.sh
@@ -2,7 +2,7 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-DIRS="confio cosmos cosmos_proto gogoproto google ibc initia tendermint"
+DIRS="confio cosmos cosmos_proto gogoproto google ibc initia tendermint proofs.d.ts proofs.js proofs.js.map"
 
 for dir in $DIRS; do
   rm -rf "$dir"
