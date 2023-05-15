759c870cdda85d7381663102975d190d16a61daf
Vritra4 <Vritra@hitel.net>
2023-05-15 16:38:33 +0900

    fix build.gradle.kts

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -24,7 +24,7 @@ allprojects {
     }
 
     protobuf {
-        generatedFilesBaseDir = "${projectDir.absolutePath}/src"
+        //generatedFilesBaseDir = "${projectDir.absolutePath}/src"
     }
 
     sourceSets {
