799033fdc5f2993ecd78458410f1c001cb2ce9ee
Vritra4 <Vritra@hitel.net>
2023-05-16 15:04:40 +0900

    interim commit for cpp

--- cpp/scripts/proto-gen.sh
+++ cpp/scripts/proto-gen.sh
@@ -19,12 +19,6 @@ INITIA_DIR="../initia/proto"
 INITIA_THIRD_PARTY_DIR="../initia/third_party/proto"
 
 protoc \
-  --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` \
-  --grpc_out="${OUT_DIR}" \
-  --proto_path="$INITIA_DIR" \
-  --proto_path="$INITIA_THIRD_PARTY_DIR" \
-  $(find ${INITIA_DIR} ${INITIA_THIRD_PARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)
-protoc \
   --cpp_out="${OUT_DIR}" \
   --proto_path="$INITIA_DIR" \
   --proto_path="$INITIA_THIRD_PARTY_DIR" \
