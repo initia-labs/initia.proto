54c2d76ce0b8f9feccb67aff242dd28ff12ee8d2
Vritra4 <Vritra@hitel.net>
2023-05-18 13:08:28 +0900

    chore fix

--- cpp/scripts/proto-gen.sh
+++ cpp/scripts/proto-gen.sh
@@ -4,11 +4,17 @@ command -v shellcheck >/dev/null && shellcheck "$0"
 
 OUT_DIR="./src"
 
-which grpc_cpp_plugin > /dev/null
+which cmake > /dev/null
 if [ $? -ne 0 ]; then
-  echo "grpc_cpp_plugin not found. Please install it first." 2>&1
+  echo "cmake not found. Please install it first." 2>&1
   exit 1
 fi
+
+#which grpc_cpp_plugin > /dev/null
+#if [ $? -ne 0 ]; then
+#  echo "grpc_cpp_plugin not found. Please install it first." 2>&1
+#  exit 1
+#fi
 
 set -o errexit -o nounset -o pipefail
 
@@ -18,6 +24,12 @@ echo "Processing initia proto files ..."
 INITIA_DIR="../initia/proto"
 INITIA_THIRD_PARTY_DIR="../initia/third_party/proto"
 
+#protoc \
+#  --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` \
+#  --grpc_out="${OUT_DIR}" \
+#  --proto_path="$INITIA_DIR" \
+#  --proto_path="$INITIA_THIRD_PARTY_DIR" \
+#  $(find ${INITIA_DIR} ${INITIA_THIRD_PARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)
 protoc \
   --cpp_out="${OUT_DIR}" \
   --proto_path="$INITIA_DIR" \
