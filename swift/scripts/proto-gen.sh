f5c89055d79a1924f359fac891a048b6559dc3b7
JSHan94 <saw151515@gmail.com>
2023-06-15 14:48:36 +0900

    change visibility

--- swift/scripts/proto-gen.sh
+++ swift/scripts/proto-gen.sh
@@ -18,7 +18,7 @@ THIRDPARTY_DIR="../third_party"
 protoc \
   --plugin=`which protoc-gen-swift` \
   --swift_out="${OUT_DIR}" \
-  --swift_opt=FileNaming=PathToUnderscores \
+  --swift_opt=Visibility=Public,FileNaming=PathToUnderscores \
   --proto_path="$INITIA_DIR" \
   --proto_path="$COSMOS_DIR" \
   --proto_path="$COSMOS_PROTO_DIR" \
@@ -29,3 +29,4 @@ protoc \
 
 # See https://github.com/apple/swift-protobuf/issues/864 
 rm "${OUT_DIR}/google_protobuf_any.pb.swift"
+
