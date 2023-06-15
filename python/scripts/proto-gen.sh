3b12c298af889149d8d4cdbd607c52a29218eee8
Vritra4 <Vritra@hitel.net>
2023-06-15 11:08:29 +0900

    bump initia for python

--- python/scripts/proto-gen.sh
+++ python/scripts/proto-gen.sh
@@ -13,13 +13,20 @@ OUT_DIR="./initia_proto"
 mkdir -p "$OUT_DIR"
 
 echo "Processing initia proto files ..."
-
 INITIA_DIR="../initia/proto"
-INITIA_THIRD_PARTY_DIR="../initia/third_party/proto"
+COSMOS_DIR="../cosmos-sdk/proto"
+COSMOS_PROTO_DIR="../cosmos-proto/proto"
+IBC_DIR="../ibc-go/proto"
+ICS_DIR="../ics23/proto"
+THIRDPARTY_DIR="../third_party"
 
 
 protoc \
-  --proto_path=${INITIA_DIR} \
-  --proto_path=${INITIA_THIRD_PARTY_DIR} \
+  --proto_path="$INITIA_DIR" \
+  --proto_path="$COSMOS_DIR" \
+  --proto_path="$COSMOS_PROTO_DIR" \
+  --proto_path="$IBC_DIR" \
+  --proto_path="$ICS_DIR" \
+  --proto_path="$THIRDPARTY_DIR" \
   --python_betterproto_out="${OUT_DIR}" \
-  $(find ${INITIA_DIR} ${INITIA_THIRD_PARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)
+  $(find ${INITIA_DIR} ${COSMOS_DIR} ${COSMOS_PROTO_DIR} ${IBC_DIR} ${ICS_DIR} ${THIRDPARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)
