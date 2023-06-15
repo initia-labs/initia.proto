ce6279d49f505c605f4d899774da809c996e7fbc
JSHan94 <saw151515@gmail.com>
2023-06-15 11:50:17 +0900

    remove any.pb.swift

--- swift/scripts/proto-gen.sh
+++ swift/scripts/proto-gen.sh
@@ -22,9 +22,11 @@ IBC_DIR="../ibc-go/proto"
 ICS_DIR="../ics23/proto"
 THIRDPARTY_DIR="../third_party"
 
+# See https://github.com/apple/swift-protobuf/issues/83
 protoc \
   --plugin=`which protoc-gen-swift` \
   --swift_out="${OUT_DIR}" \
+  --swift_opt=FileNaming=PathToUnderscores \
   --proto_path="$INITIA_DIR" \
   --proto_path="$COSMOS_DIR" \
   --proto_path="$COSMOS_PROTO_DIR" \
@@ -32,3 +34,6 @@ protoc \
   --proto_path="$ICS_DIR" \
   --proto_path="$THIRDPARTY_DIR" \
   $(find ${INITIA_DIR} ${COSMOS_DIR} ${COSMOS_PROTO_DIR} ${IBC_DIR} ${ICS_DIR} ${THIRDPARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)
+
+# See https://github.com/apple/swift-protobuf/issues/864 
+rm "${OUT_DIR}/google_protobuf_any.pb.swift"
