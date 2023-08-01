e6307f2782122328b91dea885be2c83f508c8d9a
Vritra4 <Vritra@hitel.net>
2023-08-01 12:49:25 +0900

    js: support pob

--- scripts/getver.awk
+++ scripts/getver.awk
@@ -1,1 +1,4 @@
-{ if ($1 == expr) { print $2; } }
+{ 
+	if ($1 == expr) { print $2; } 
+	else if ($1 == "require" && $2 == expr) { print $3; }
+}
--- scripts/update-submodule.sh
+++ scripts/update-submodule.sh
@@ -11,6 +11,7 @@ COSMOS_PROTO_URL=github.com/cosmos/cosmos-proto
 IBC_URL=github.com/cosmos/ibc-go
 ICS_URL=github.com/confio/ics23/go
 IBC_V=v7
+POB_URL=github.com/skip-mev/pob
 
 GOMOD=$1/initia/go.mod
 AWK_SCRIPT=$1/scripts/getver.awk
@@ -19,6 +20,7 @@ COSMOS_SDK_VERSION=$(awk -f $AWK_SCRIPT -v expr="$COSMOS_URL" $GOMOD)
 COSMOS_PROTO_VERSION=$(awk -f $AWK_SCRIPT -v expr="$COSMOS_PROTO_URL" $GOMOD)
 IBC_VERSION=$(awk -f $AWK_SCRIPT -v expr="$IBC_URL/$IBC_V" $GOMOD)
 ICS_VERSION=$(awk -f $AWK_SCRIPT -v expr="$ICS_URL" $GOMOD)
+POB_VERSION=$(awk -f $AWK_SCRIPT -v expr="$POB_URL" $GOMOD)
 
 # if ICS_VERSION is v0.9.0, forced to set v0.10.0
 if [[ "$ICS_VERSION" == "v0.9.0" ]]; then
@@ -31,6 +33,7 @@ echo "COSMOS_SDK_VERSION: $COSMOS_SDK_VERSION"
 echo "COSMOS_PROTO_VERSION: $COSMOS_PROTO_VERSION"
 echo "IBC_VERSION: $IBC_VERSION"
 echo "ICS_VERSION: $ICS_VERSION"
+echo "POB_VERSION: $POB_VERSION"
 
 # they don't have branches for their releases. use tags instead
 pushd .
@@ -38,6 +41,7 @@ cd $1/cosmos-sdk; git checkout $COSMOS_SDK_VERSION
 cd $1/cosmos-proto; git checkout $COSMOS_PROTO_VERSION
 cd $1/ibc-go; git checkout $IBC_VERSION
 cd $1/ics23; git checkout go/$ICS_VERSION    # ics23's tag have a prefix "go/"
+cd $1/pob; git checkout $POB_VERSION
 popd
 
 
