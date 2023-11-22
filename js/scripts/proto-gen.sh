1c6ad86039a9b0717e85b80fd231c7ae49019f80
joon9823 <joon9823@gmail.com>
2023-11-22 16:50:10 +0900

    Remove pob

--- js/scripts/proto-gen.sh
+++ js/scripts/proto-gen.sh
@@ -15,7 +15,6 @@ COSMOS_DIR="../cosmos-sdk/proto"
 COSMOS_PROTO_DIR="../cosmos-proto/proto"
 IBC_DIR="../ibc-go/proto"
 ICS_DIR="../ics23/proto"
-POB_DIR="../pob/proto"
 WASMD_DIR="../wasmd/proto"
 SDK_DIR="../block-sdk/proto"
 THIRDPARTY_DIR="../third_party"
@@ -29,8 +28,7 @@ protoc \
   --proto_path="$COSMOS_PROTO_DIR" \
   --proto_path="$IBC_DIR" \
   --proto_path="$ICS_DIR" \
-  --proto_path="$POB_DIR" \
   --proto_path="$WASMD_DIR" \
   --proto_path="$SDK_DIR" \
   --proto_path="$THIRDPARTY_DIR" \
-  $(find ${INITIA_DIR} ${COSMOS_DIR} ${COSMOS_PROTO_DIR} ${IBC_DIR} ${ICS_DIR} ${POB_DIR} ${WASMD_DIR} ${SDK_DIR} ${THIRDPARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)
+  $(find ${INITIA_DIR} ${COSMOS_DIR} ${COSMOS_PROTO_DIR} ${IBC_DIR} ${ICS_DIR} ${WASMD_DIR} ${SDK_DIR} ${THIRDPARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)
--- scripts/update-submodule.sh
+++ scripts/update-submodule.sh
@@ -11,7 +11,6 @@ COSMOS_PROTO_URL=github.com/cosmos/cosmos-proto
 IBC_URL=github.com/cosmos/ibc-go
 ICS_URL=github.com/confio/ics23/go
 IBC_V=v7
-POB_URL=github.com/skip-mev/pob
 WASMD_URL=github.com/CosmWasm/wasmd
 BLOCK_SDK_URL=github.com/skip-mev/block-sdk
 
@@ -23,7 +22,6 @@ COSMOS_SDK_VERSION=$(awk -f $AWK_SCRIPT -v expr="$COSMOS_URL" $GOMOD)
 COSMOS_PROTO_VERSION=$(awk -f $AWK_SCRIPT -v expr="$COSMOS_PROTO_URL" $GOMOD)
 IBC_VERSION=$(awk -f $AWK_SCRIPT -v expr="$IBC_URL/$IBC_V" $GOMOD)
 ICS_VERSION=$(awk -f $AWK_SCRIPT -v expr="$ICS_URL" $GOMOD)
-POB_VERSION=$(awk -f $AWK_SCRIPT -v expr="$POB_URL" $GOMOD)
 WASMD_VERSION=$(awk -f $AWK_SCRIPT -v expr="$WASMD_URL" $MINIGOMOD)
 BLOCK_SDK_VERSION=$(awk -f $AWK_SCRIPT -v expr="$BLOCK_SDK_URL" $GOMOD) 
 
@@ -38,7 +36,6 @@ echo "COSMOS_SDK_VERSION: $COSMOS_SDK_VERSION"
 echo "COSMOS_PROTO_VERSION: $COSMOS_PROTO_VERSION"
 echo "IBC_VERSION: $IBC_VERSION"
 echo "ICS_VERSION: $ICS_VERSION"
-echo "POB_VERSION: $POB_VERSION"
 echo "WASMD_VERSION: $WASMD_VERSION"
 echo "BLOCK_SDK_VERSION: $BLOCK_SDK_VERSION"
 
@@ -48,7 +45,6 @@ cd $1/cosmos-sdk; git checkout $COSMOS_SDK_VERSION
 cd $1/cosmos-proto; git checkout $COSMOS_PROTO_VERSION
 cd $1/ibc-go; git checkout $IBC_VERSION
 cd $1/ics23; git checkout go/$ICS_VERSION    # ics23's tag have a prefix "go/"
-cd $1/pob; git checkout $POB_VERSION
 cd $1/wasmd; git checkout $WASMD_VERSION
 cd $1/block-sdk; git checkout $BLOCK_SDK_VERSION
 popd
