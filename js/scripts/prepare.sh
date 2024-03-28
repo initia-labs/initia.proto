a03ed123e70ac83e8a072439ab41cac876ee5b5f
joon9823 <joon9823@gmail.com>
2024-03-28 15:24:26 +0900

    Add celestia module

--- js/scripts/prepare.sh
+++ js/scripts/prepare.sh
@@ -2,7 +2,7 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-DIRS="amino cosmos cosmos_proto gogoproto google ibc initia tendermint cosmwasm sdk slinky miniwasm minievm"
+DIRS="amino cosmos cosmos_proto gogoproto google ibc initia tendermint cosmwasm sdk slinky miniwasm minievm celestia"
 
 for dir in $DIRS; do
   rm -rf "$dir"
